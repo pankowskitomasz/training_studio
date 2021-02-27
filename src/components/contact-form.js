5afc9468c57918f4733f3b92ca8df26a3700f85d
pankowskitomasz <pankowski_tomasz@o2.pl>
2021-02-27 04:35:12 +0100

    added backend

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,17 +1,135 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
 import Row from "../../node_modules/react-bootstrap/Row";
 import Col from "../../node_modules/react-bootstrap/Col";
 import Button from "../../node_modules/react-bootstrap/Button";
 import Form from "../../node_modules/react-bootstrap/Form";
+import update from "react-addons-update";
+import {APP_LINKS} from "../config";
 
-class ContactForm extends Component{
-    render(){
-        return(        
-            <Container fluid className={"contact-form align-items-center p-0 py-5 bg-white d-flex minh-50vh "+this.props.classExt}>
+class ContactForm extends Component {
+    constructor() {
+        super();
+        this.state = {
+            formData: {
+                emailAddress: "",
+                firstName: "",
+                lastName: "",
+                messageText: "",
+                phoneNumber: ""
+            }
+        };
+    }
+    updateFormData(ev) {
+        var updateData;
+        if (ev.target.name === "firstName") {
+            updateData = update(this.state.formData, {
+                firstName: { $set: ev.target.value }
+            });
+            this.setState({ formData: updateData });
+        }
+        else if (ev.target.name === "lastName") {
+            updateData = update(this.state.formData, {
+                lastName: { $set: ev.target.value }
+            });
+            this.setState({ formData: updateData });
+        }
+        else if (ev.target.name === "emailAddress") {
+            updateData = update(this.state.formData, {
+                emailAddress: { $set: ev.target.value }
+            });
+            this.setState({ formData: updateData });
+        }
+        else if (ev.target.name === "phoneNumber") {
+            updateData = update(this.state.formData, {
+                phoneNumber: { $set: ev.target.value }
+            });
+            this.setState({ formData: updateData });
+        }
+        else if (ev.target.name === "messageText") {
+            updateData = update(this.state.formData, {
+                messageText: { $set: ev.target.value }
+            });
+            this.setState({ formData: updateData });
+        }
+    }
+
+    getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+          }
+        }
+        return "";
+      }
+
+    setCookie(cname, cvalue, exh) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exh*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    sendForm(){
+        if(this.state.formData.firstName.length!==0
+        && this.state.formData.lastName.length!==0
+        && this.state.formData.emailAddress.length!==0
+        && this.state.formData.phoneNumber.length!==0
+        && this.state.formData.messageText.length!==0){
+            let formData = new FormData();
+            formData.append("fname",this.state.formData.firstName);
+            formData.append("flast",this.state.formData.lastName);
+            formData.append("fmail",this.state.formData.emailAddress);
+            formData.append("fphone",this.state.formData.phoneNumber);
+            formData.append("fmsg",this.state.formData.messageText);
+            let msgcnt = this.getCookie("msgcount");
+            msgcnt = (msgcnt==="")?0:msgcnt;
+            formData.append("msgcount",msgcnt);
+            fetch(APP_LINKS.messages,{
+                method:"POST",
+                body:formData
+            })
+            .then((response)=>response.json())
+            .then((data)=>{
+                if(data["msgcount"]!==undefined
+                && !isNaN(data["msgcount"])){                    
+                    this.setCookie("msgcount",data.msgcount,2);
+                    this.props.backNav("confirm");
+                }
+            })
+            .catch((error)=>{
+                this.props.backNav("error");
+            });  
+            this.clearForm();
+        }
+    }
+
+    clearForm(){
+        let clearData = update(this.state.formData,{
+            emailAddress: {$set: ""},
+            firstName: {$set:""},
+            lastName: {$set:""},
+            messageText: {$set:""},
+            phoneNumber: {$set:""}
+        });
+        this.setState({ formData: clearData });        
+    }
+
+    render() {
+        return (
+            <Container fluid className={"contact-form align-items-center p-0 py-5 bg-white d-flex minh-50vh " + this.props.classExt}>
                 <Row className="mx-auto text-center w-100">
                     <Col xs={10} md={6} className="mx-auto p-0 d-flex align-items-center">
-                        <Form className="text-left w-100 border border-secondary p-4 rounded bg-white shadow text-secondary font-weight-bold">
+                        <Form className="text-left w-100 border border-secondary p-4 rounded bg-white shadow text-secondary font-weight-bold"
+                            autoComplete="off"
+                            method="POST">
                             <div className="border-bottom mb-4">
                                 <p className="font-weight-bold mb-1">
                                     Contact online
@@ -19,36 +137,75 @@ class ContactForm extends Component{
                             </div>
                             <Form.Group controlId="formFirstName">
                                 <Form.Label>First Name</Form.Label>
-                                 <Form.Control type="text" placeholder="Enter first name" className="rounded-pill"/>
+                                <Form.Control type="text"
+                                    placeholder="Enter first name"
+                                    className="rounded-pill"
+                                    maxLength="50"
+                                    name="firstName"
+                                    onChange={this.updateFormData.bind(this)} 
+                                    value={this.state.formData.firstName}
+                                    required/>
                             </Form.Group>
                             <Form.Group controlId="formLastName">
                                 <Form.Label>Last Name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter last name" className="rounded-pill"/>
+                                <Form.Control type="text"
+                                    placeholder="Enter last name"
+                                    className="rounded-pill"
+                                    maxLength="50"
+                                    name="lastName"
+                                    onChange={this.updateFormData.bind(this)} 
+                                    value={this.state.formData.lastName}
+                                    required/>
                             </Form.Group>
                             <Form.Group controlId="formEmail">
                                 <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" className="rounded-pill"/>
+                                <Form.Control type="email"
+                                    placeholder="Enter email"
+                                    className="rounded-pill"
+                                    maxLength="50"
+                                    name="emailAddress"
+                                    onChange={this.updateFormData.bind(this)} 
+                                    value={this.state.formData.emailAddress}
+                                    required/>
                             </Form.Group>
                             <Form.Group controlId="formPhone">
                                 <Form.Label>Phone</Form.Label>
-                                <Form.Control type="tel" placeholder="Enter phone" className="rounded-pill"/>
+                                <Form.Control type="tel"
+                                    placeholder="Enter phone"
+                                    className="rounded-pill"
+                                    maxLength="15"
+                                    name="phoneNumber"
+                                    onChange={this.updateFormData.bind(this)} 
+                                    value={this.state.formData.phoneNumber}
+                                    required/>
                             </Form.Group>
                             <Form.Group controlId="formMessage">
                                 <Form.Label>Message</Form.Label>
-                                <Form.Control as="textarea" rows={4} />
+                                <Form.Control as="textarea"
+                                    rows={4}
+                                    maxLength="250"
+                                    name="messageText"
+                                    onChange={this.updateFormData.bind(this)} 
+                                    value={this.state.formData.messageText}
+                                    required/>
                             </Form.Group>
                             <div className="w-100 text-center">
-                                <Button variant="outline-dark" type="reset" className="mx-1 rounded-pill">
+                                <Button variant="outline-dark"
+                                    type="reset"
+                                    className="mx-1 rounded-pill">
                                     Clear
                                 </Button>
-                                <Button variant="outline-dark" type="submit" className="mx-1 rounded-pill">
+                                <Button
+                                    variant="outline-dark"
+                                    className="mx-1 rounded-pill"
+                                    onClick={this.sendForm.bind(this)}>
                                     Send
                                 </Button>
                             </div>
                         </Form>
                     </Col>
                 </Row>
-            </Container>    
+            </Container>
         );
     }
 }
--- src/views/login.js
+++ src/views/login.js
@@ -1,13 +1,43 @@
 import React,{Component} from "react";
 import LoginForm from "../components/login-form";
 import SpacerTop from "../components/spacer-top";
+import LoginError from "../components/login-error";
+import Dashboard from "../components/dashboard";
+import MessagesList from "../components/message-list";
+import update from "react-addons-update";
 
 class Login extends Component{
+    constructor(){
+        super();
+        this.state={
+            navForm:"form"
+        };
+    }
+    hndViewChange(ev){
+        let upd = update(this.state,{navForm:{$set: ev}});
+        this.setState(upd);
+    }
     render(){
+        let content;
+        if(this.state.navForm==="form"){
+            content = <LoginForm backNav={this.hndViewChange.bind(this)}/>;
+        }
+        else if(this.state.navForm==="dashboard"){
+            content = <Dashboard backNav={this.hndViewChange.bind(this)}/>
+        }
+        else if(this.state.navForm==="messages"){
+            content = <MessagesList backNav={this.hndViewChange.bind(this)}/>
+        }
+        else if(this.state.navForm==="logout"){
+            content = <LoginForm backNav={this.hndViewChange.bind(this)}/>;
+        }
+        else{
+            content = <LoginError backNav={this.hndViewChange.bind(this)}/>
+        }
         return(        
             <main className="minh-100vh p-header bg-white">
                 <SpacerTop/>
-                <LoginForm/>
+                {content}
             </main>
         );
     }
